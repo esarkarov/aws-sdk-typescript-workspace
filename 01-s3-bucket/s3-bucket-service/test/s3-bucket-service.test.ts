24a9f9ffd175e815625f1f34f283f37b61480d83
Elvin <elvinsrkrov@gmail.com>
2026-01-12 20:49:31 +0400

    test: add unit tests for S3 bucket service stack

--- 01-s3-bucket/s3-bucket-service/test/s3-bucket-service.test.ts
+++ 01-s3-bucket/s3-bucket-service/test/s3-bucket-service.test.ts
@@ -1,17 +1,57 @@
-// import * as cdk from 'aws-cdk-lib/core';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as S3BucketService from '../lib/s3-bucket-service-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { S3BucketServiceStack } from "../lib/s3-bucket-service-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/s3-bucket-service-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new S3BucketService.S3BucketServiceStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("S3BucketServiceStack", () => {
+  let app: cdk.App;
+  let stack: S3BucketServiceStack;
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new S3BucketServiceStack(app, "TestStack");
+    template = Template.fromStack(stack);
+  });
+
+  test("creates two S3 buckets", () => {
+    template.resourceCountIs("AWS::S3::Bucket", 2);
+  });
+
+  test("primary bucket is created", () => {
+    const buckets = template.findResources("AWS::S3::Bucket");
+    expect(Object.keys(buckets).length).toBeGreaterThanOrEqual(1);
+  });
+
+  test("website bucket has public access enabled", () => {
+    template.hasResourceProperties("AWS::S3::Bucket", {
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: false,
+        BlockPublicPolicy: false,
+        IgnorePublicAcls: false,
+        RestrictPublicBuckets: false,
+      },
+      WebsiteConfiguration: {
+        IndexDocument: "index.html",
+      },
+    });
+  });
+
+  test("creates three CloudFormation outputs", () => {
+    const outputs = template.findOutputs("*");
+    expect(Object.keys(outputs)).toHaveLength(3);
+  });
+
+  test("outputs contain bucket names and website URL", () => {
+    template.hasOutput("PrimaryBucketName", {
+      Description: "The name of the primary bucket",
+    });
+
+    template.hasOutput("WebsiteBucketName", {
+      Description: "The name of the website bucket",
+    });
+
+    template.hasOutput("WebsiteUrl", {
+      Description: "The website URL",
+    });
+  });
 });
